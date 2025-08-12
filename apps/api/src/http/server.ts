71803f1b497e5d5c0f231792ac0a7b4ca202a9eb
Filipe <filiperotherds@gmail.com>
2025-08-12 13:19:20 -0300

    Rotas: Listagem e detalhe da organização

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -19,6 +19,8 @@ import { resetPassword } from './routes/auth/reset-password'
 import { env } from '@saas/env'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getMemberhip } from './routes/orgs/get-membership'
+import { getOrganization } from './routes/orgs/get-organization'
+import { getOrganizations } from './routes/orgs/get-organizations'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -65,6 +67,8 @@ app.register(resetPassword)
 
 app.register(createOrganization)
 app.register(getMemberhip)
+app.register(getOrganization)
+app.register(getOrganizations)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
