e1b7ebbf5f57961752765f0281d038419468716b
Filipe <filiperotherds@gmail.com>
2025-08-18 13:37:07 -0300

    add security bearerAuth

--- apps/api/src/http/routes/invites/get-pending-invites.ts
+++ apps/api/src/http/routes/invites/get-pending-invites.ts
@@ -17,6 +17,7 @@ export async function getPendingInvites(app: FastifyInstance) {
         schema: {
           tags: ['invites'],
           summary: 'Get all user pending invites',
+          security: [{ bearerAuth: [] }],
           response: {
             200: z.object({
               invites: z.array(
